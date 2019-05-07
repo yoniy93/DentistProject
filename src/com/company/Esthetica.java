8d073fc7ca93ec3e361e4c3b275b5aa4e5bac82d
Yonatan Yardeni <yoniy93@gmail.com>
2019-05-07 18:00:15 +0300

    yonatan check

--- src/com/company/Esthetica.java
+++ src/com/company/Esthetica.java
@@ -4,5 +4,6 @@ public class Esthetica extends Treatments {
 
     public Esthetica(int treatmentCode, String treatmentName, int duration, int cost) {
         //super(treatmentCode, treatmentName, duration, cost);
+        int x;
     }
 }
