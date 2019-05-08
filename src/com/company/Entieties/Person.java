6f7db86f5d2036f816e399040387e55b94a7f383
Yonatan Yardeni <yoniy93@gmail.com>
2019-05-08 09:58:18 +0300

    second commit

---
+++ src/com/company/Entieties/Person.java
@@ -0,0 +1,12 @@
+package com.company.Entieties;
+
+public abstract class Person {
+
+    private String lastName;
+    private String firstName;
+    private int weight;
+    private int height;
+    private String gender;
+
+
+}
---
+++ src/com/company/Entieties/User.java
@@ -0,0 +1,7 @@
+package com.company.Entieties;
+
+public abstract class User extends Person {
+    private String username;
+    private String password;
+
+}
