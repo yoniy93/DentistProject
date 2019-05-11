a60d7d4f88200839e3a4ec875c63b7992b01b425
OfirGan <OfirGan@users.noreply.github.com>
2019-05-11 20:44:14 +0300

    Ofir The King

--- src/project/Entieties/User.java
+++ src/project/Entieties/User.java
@@ -3,5 +3,32 @@ package project.Entieties;
 public abstract class User extends Person {
     private String username;
     private String password;
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
 
 }
