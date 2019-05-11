9c72af397c7364792f261674a905d2ba759a179c
amitaflalo <amitaflalo01@gmail.com>
2019-05-11 20:17:19 +0300

    setter and getter

--- src/project/Entieties/Person.java
+++ src/project/Entieties/Person.java
@@ -7,6 +7,57 @@ public abstract class Person {
     private int weight;
     private int height;
     private String gender;
+    private Date dateOfBirth;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+
 
 
 }
