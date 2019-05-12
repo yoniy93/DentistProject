cf2e87a3c440071fcd21fd10155771ff3e02b3f1
amitaflalo <amitaflalo01@gmail.com>
2019-05-12 13:34:24 +0300

    שינוי שם לproject

---
+++ src/project/Entieties/EquipmentPerTreatment.java
@@ -0,0 +1,4 @@
+package project.Entieties;
+
+public class EquipmentPerTreatment extends MedicalEquipment {
+}
--- src/project/Entieties/MedicalEquipment.java
+++ src/project/Entieties/MedicalEquipment.java
@@ -6,6 +6,39 @@ public abstract class MedicalEquipment {
 
     private Date expirationDate;
     private int id;
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmountInStock() {
+        return amountInStock;
+    }
+
+    public void setAmountInStock(int amountInStock) {
+        this.amountInStock = amountInStock;
+    }
+
     private String name;
     private int amountInStock;
 }
