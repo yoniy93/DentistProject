b002e7abc1c7dfcf8fb507d4884cbf31d2a28310
amitaflalo <amitaflalo01@gmail.com>
2019-05-12 14:53:49 +0300

    שינוי שם לproject

--- src/project/Entities/Appointment.java
+++ src/project/Entities/Appointment.java
@@ -8,4 +8,49 @@ public class Appointment {
     private Treatments treatmentType;
     private Time treatmentTime ;
     private Date treatmentDate;
+    private Doctor  doctor;
+    private Patient patient;
+
+    public Treatments getTreatmentType() {
+        return treatmentType;
+    }
+
+    public void setTreatmentType(Treatments treatmentType) {
+        this.treatmentType = treatmentType;
+    }
+
+    public Time getTreatmentTime() {
+        return treatmentTime;
+    }
+
+    public void setTreatmentTime(Time treatmentTime) {
+        this.treatmentTime = treatmentTime;
+    }
+
+    public Date getTreatmentDate() {
+        return treatmentDate;
+    }
+
+    public void setTreatmentDate(Date treatmentDate) {
+        this.treatmentDate = treatmentDate;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+
+
 }
