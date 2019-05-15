04d64357b2f8dfd80b089dd69fdebd45649f9f81
Yonatan Yardeni <yoniy93@gmail.com>
2019-05-15 19:32:55 +0300

    Merge branch 'master' of /Users/yonatan/IdeaProjects/DentistProject with conflicts.

--- src/project/Screens/UI.java
+++ src/project/Screens/UI.java
@@ -3,42 +3,24 @@ package project.Screens;
 import javax.swing.*;
 
 public class UI {
-    JFrame frame;
-    JLabel label;
-    JButton button;
-    MyListener listener;
-    JCheckBox checkBox;
-    public UI (){
-        frame= new JFrame();
+
+    public static void main (String[] args){
+        JFrame frame= new JFrame();
         frame.setBounds(100,100,800,600);
         frame.setLayout(null);
         frame.setVisible(true);
 
-         label= new JLabel();
+        JLabel label = new JLabel();
         label.setBounds(100,100,200,60);
-        label.setText("hello amit");
+        label.setText("hello amir");
         frame.add(label);
 
-        button=new JButton();
+        JButton button=new JButton();
         button.setBounds(100,200,200,60);
         button.setText("click me");
-        listener=new MyListener();
+        MyListener listener=new MyListener();
         button.addActionListener(listener);
         frame.add(button);
-
-        checkBox =new JCheckBox();
-        checkBox.setBounds(300,100,200,60);
-        checkBox.setText("yes or no");
-        frame.add(checkBox);
-    }
-
-    public void update() {
-        label.setText("number of clicks" + listener.numberOfClicked);
-
-    }
-    public static void main(String[] args) {
-         UI ui=new UI();
-         ui.update();
 
     }
 }
