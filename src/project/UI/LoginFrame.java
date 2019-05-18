378e0f1df34e460f5f31bfd7aae6e9d14edada39
OfirGan <OfirGan@users.noreply.github.com>
2019-05-18 16:06:34 +0300

    Ofir :)

---
+++ src/project/UI/LoginFrame.java
@@ -0,0 +1,98 @@
+package project.UI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginFrame extends JFrame implements ActionListener {
+
+    private Container container = getContentPane();
+    private JLabel userLabel = new JLabel("USERNAME");
+    private JLabel passwordLabel = new JLabel("PASSWORD");
+    private JTextField userTextField = new JTextField();
+    private JPasswordField passwordField = new JPasswordField();
+    private JButton loginButton = new JButton("LOGIN");
+    private JButton resetButton = new JButton("RESET");
+    private JCheckBox showPassword = new JCheckBox("Show Password");
+
+
+    LoginFrame() {
+        setLayoutManager();
+        setLocationAndSize();
+        addComponentsToContainer();
+        addActionEvent();
+
+        setTitle("Login Form");
+        setVisible(true);
+        setBounds(500, 100, 370, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+    }
+
+    private void setLayoutManager() {
+        container.setLayout(null);
+    }
+
+    private void setLocationAndSize() {
+        userLabel.setBounds(50, 150, 100, 30);
+        passwordLabel.setBounds(50, 220, 100, 30);
+        userTextField.setBounds(150, 150, 150, 30);
+        passwordField.setBounds(150, 220, 150, 30);
+        showPassword.setBounds(150, 250, 150, 30);
+        loginButton.setBounds(50, 300, 100, 30);
+        resetButton.setBounds(200, 300, 100, 30);
+    }
+
+    private void addComponentsToContainer() {
+        container.add(userLabel);
+        container.add(passwordLabel);
+        container.add(userTextField);
+        container.add(passwordField);
+        container.add(showPassword);
+        container.add(loginButton);
+        container.add(resetButton);
+    }
+
+    private void addActionEvent() {
+        loginButton.addActionListener(this);
+        resetButton.addActionListener(this);
+        showPassword.addActionListener(this);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //Coding Part of LOGIN button
+        if (e.getSource() == loginButton) {
+            String userText;
+            String pwdText;
+            userText = userTextField.getText();
+            pwdText =  new String(passwordField.getPassword());
+
+            if (userText.equalsIgnoreCase("admin") && pwdText.equalsIgnoreCase("admin")) {
+                //JOptionPane.showMessageDialog(this, "Login Successful");
+                AdminFrame adminFrame = new AdminFrame();
+                setVisible(false);
+            }
+            else {
+                JOptionPane.showMessageDialog(this, "Invalid Username or Password");
+            }
+        }
+
+        //Coding Part of RESET button
+        if (e.getSource() == resetButton) {
+            userTextField.setText("");
+            passwordField.setText("");
+        }
+
+        //Coding Part of showPassword JCheckBox
+        if (e.getSource() == showPassword) {
+            if (showPassword.isSelected()) {
+                passwordField.setEchoChar((char) 0);
+            } else {
+                passwordField.setEchoChar('*');
+            }
+        }
+    }
+}
