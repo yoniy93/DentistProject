874b2b5dc90795ac1a67063d7640cf95d8e489ed
OfirGan <OfirGan@users.noreply.github.com>
2019-05-25 18:25:55 +0300

    Ofir :)

--- src/project/GUI/AdminModel.java
+++ src/project/GUI/AdminModel.java
@@ -1,4 +1,12 @@
 package project.GUI;
 
+import project.Entities.Admin;
+
 public class AdminModel {
+
+    private Admin admin;
+
+    public AdminModel(Admin admin){
+        this.admin = admin;
+    }
 }
--- src/project/GUI/StartAdminView.java
+++ src/project/GUI/StartAdminView.java
@@ -4,7 +4,7 @@ import project.Entities.Admin;
 
 public class StartAdminView {
     public StartAdminView(Admin admin){
-        AdminModel adminM = new AdminModel();
+        AdminModel adminM = new AdminModel(admin);
         AdminView adminV = new AdminView();
         new AdminController(adminM, adminV);
     }
