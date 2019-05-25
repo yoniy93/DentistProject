e37cb2a4e7e0d727895adfc8ea394158460aa111
Yonatan Yardeni <yoniy93@gmail.com>
2019-05-25 18:22:34 +0300

    לוגיקה לכניסה

--- src/project/GUI/Main.java
+++ src/project/GUI/Main.java
@@ -1,12 +1,18 @@
 package project.GUI;
 
 
+import project.Entities.USER_TYPE;
+import project.Logic.DatabaseHandler;
+
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args)
     {
 
+        DatabaseHandler.createNewDatabase();
+        DatabaseHandler.createNewTables();
+        new DatabaseHandler().insert("1","1","a","a","e",1,1,"13:05:93", USER_TYPE.ADMIN,"M");
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new StartLoginView();
