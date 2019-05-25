2b07cf5ec287470234631763e428225a2001752c
amitaflalo <amitaflalo01@gmail.com>
2019-05-25 18:28:56 +0300

    שינוי שם לproject

--- src/project/GUI/PatientModel.java
+++ src/project/GUI/PatientModel.java
@@ -1,5 +1,13 @@
 package project.GUI;
 
+import project.Entities.Patient;
+
 public class PatientModel {
+
+    private Patient patient;
+    public PatientModel(Patient patient)
+    {
+        this.patient=patient;
+    }
 
 }
