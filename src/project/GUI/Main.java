e6edb509a4b7970ef61e550f176c092e1c661680
OfirGan <OfirGan@users.noreply.github.com>
2019-05-26 01:27:30 +0300

    Ofir :)

--- src/project/GUI/Main.java
+++ src/project/GUI/Main.java
@@ -10,15 +10,21 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args)
     {
-        DatabaseHandler a = new DatabaseHandler();
-        DatabaseHandler.createNewDatabase();
-        DatabaseHandler.createNewTables();
-        a.insert("1","1","a","a","e",1,1,"130593", USER_TYPE.DOCTOR,"M",1);
+        CreateDataBase();
 
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new StartLoginView();
             }
         });
+    }
+
+    public static void CreateDataBase(){
+
+        DatabaseHandler a = new DatabaseHandler();
+        DatabaseHandler.createNewDatabase();
+        DatabaseHandler.createNewTables();
+        a.insert("1","1","a","a","e",1,1,"130593", USER_TYPE.DOCTOR,"M",1);
+        a.insert("2","2","a","a","e",1,1,"130593", USER_TYPE.PATIENT,"M",1);
     }
 }
