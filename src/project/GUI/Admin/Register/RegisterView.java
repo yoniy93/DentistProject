513c8383b607048c043f559faa0090a8b55f45db
OfirGan <OfirGan@users.noreply.github.com>
2019-05-28 17:18:07 +0300

    Ofir :)

--- src/project/GUI/Admin/Register/RegisterView.java
+++ src/project/GUI/Admin/Register/RegisterView.java
@@ -58,7 +58,7 @@ public class RegisterView extends JFrame
 
         setTitle("Register New User");
         setBounds(300, 100, 800, 800);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setResizable(false);
         setVisible(true);
     }
