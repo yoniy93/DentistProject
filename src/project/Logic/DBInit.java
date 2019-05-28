2db3b10b4d79bf3e69b914be1228eb146949d362
Yonatan Yardeni <yoniy93@gmail.com>
2019-05-28 12:12:53 +0300

    לוגיקה לכניסה

--- src/project/Logic/DBInit.java
+++ src/project/Logic/DBInit.java
@@ -4,11 +4,12 @@ import java.io.File;
 
 public class DBInit {
 
-    private static boolean isDBExists(){
+    private static boolean isDBExists() {
         File file=new File(DBLocation.DBLocation());
         if (file.exists())
             return true;
-        else return false;
+        else
+            return false;
     }
 
     private static void createTablesInDB(){
@@ -21,7 +22,10 @@ public class DBInit {
         dbInserts.insertForAdmin("1234567", "1234567", "Admin", "Admin", "mail", "13-05-1993", "male");
         dbInserts.insertForDoctor("302208178", "12345", "Yoni", "Yardeni", "yoniy93@gmail.com", "13-05-1993", "male", 3);
         dbInserts.insertForPatient("123", "123", "check", "check", "check", 60.5, 178, "13-05-1993", "male");
-        dbInserts.insertTreatment(1, "שיננית", 50, 100);
+        dbInserts.insertTreatment(1, "שיננית", 30, 70.5);
+        dbInserts.insertTreatment(2, "סתימה", 60, 99);
+        dbInserts.insertTreatment(3, "גשר", 90, 150);
+        dbInserts.insertTreatment(4, "שיננית", 60, 69.9);
         dbInserts.insertMedicalEquipment(11, "מזלג", "12-12-2020", 4);
     }
 
