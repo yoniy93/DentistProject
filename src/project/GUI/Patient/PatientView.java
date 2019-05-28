41ae358b3416f43aa4189175a4d0b5293376dfb3
amitaflalo <amitaflalo01@gmail.com>
2019-05-28 09:20:44 +0300

    שינוי שם לproject

--- src/project/GUI/Patient/PatientView.java
+++ src/project/GUI/Patient/PatientView.java
@@ -1,15 +1,40 @@
 package project.GUI.Patient;
+import jdk.tools.jlink.internal.Jlink;
 import project.GUI.PersonView;
 
 import javax.swing.*;
 
 public class PatientView extends PersonView {
 
-    private JButton editPersonalDetails;
+    private JButton setAnAppointment=new JButton("Set An Appointment ");
 
     public PatientView()
     {
+        setLayout(null);
+        setLocationAndSize();
+        addComponentsToContainer();
+        addPatientToContainer();
+        setPatient();
+
         setTitle("WELCOME PATIENT");
+        setBounds(500, 100, 800, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
     }
+
+    void addPatientToContainer()
+    {
+        add(setAnAppointment);
+
+    }
+    void setPatient()
+    {
+        setAnAppointment.setBounds(150, 220, 150, 30);
+    }
+    public JButton getSetAnAppointment() {
+        return setAnAppointment;
+    }
+
 
 }
