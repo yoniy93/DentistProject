2dc042ec73e44cf7506a5a73971beaa2f679ea18
OfirGan <OfirGan@users.noreply.github.com>
2019-05-29 21:13:17 +0300

    Ofir :)

--- src/project/GUI/ClinicStaffInfo/ClinicStaffInfoView.java
+++ src/project/GUI/ClinicStaffInfo/ClinicStaffInfoView.java
@@ -2,34 +2,30 @@ package project.GUI.ClinicStaffInfo;
 
 import javax.swing.*;
 
+
 public class ClinicStaffInfoView extends JFrame {
 /*
-    protected PersonView(){
+     calendar = new JCalendar();
+    JDateChooser chooser = new JDateChooser();
+
+
+    public ClinicStaffInfoView(){
         setLayout(null);
         setLocationAndSize();
         addComponentsToFrame();
 
         setBounds(500, 100, 800, 600);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setResizable(false);
         setVisible(true);
     }
 
-    public PersonView getPersonView() {
-        return this;
-    }
-
     private void setLocationAndSize() {
-        logoutButton.setBounds(10, 10, 100, 30);
-        editPersonalDetails.setBounds(150, 290, 150, 30);
-        viewClinicStaffInfo.setBounds(150,360,150,30);
 
     }
 
     private void addComponentsToFrame() {
-        add(logoutButton);
-        add(editPersonalDetails);
-        add(viewClinicStaffInfo);
+
     }
 */
 }
