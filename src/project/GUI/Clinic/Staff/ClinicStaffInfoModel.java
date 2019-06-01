c1acd9c1c5f22afb4fc5147082d787e349c1f40a
amitaflalo <amitaflalo01@gmail.com>
2019-06-01 11:15:18 +0300

    שינוי שם לproject

--- src/project/GUI/Clinic/Staff/ClinicStaffInfoModel.java
+++ src/project/GUI/Clinic/Staff/ClinicStaffInfoModel.java
@@ -1,4 +1,17 @@
 package project.GUI.Clinic.Staff;
 
+import project.Database.DBQuerys;
+import project.Entities.Doctor;
+
+import java.util.Vector;
+
 public class ClinicStaffInfoModel {
+
+    DBQuerys dbQuerys =new DBQuerys();;
+
+    public void createDoctorsTable()
+    {
+        Vector<Doctor> doctors=new Vector<Doctor>();
+        doctors=dbQuerys.getDoctors();
+    }
 }
--- src/project/GUI/Clinic/Staff/ClinicStaffInfoView.java
+++ src/project/GUI/Clinic/Staff/ClinicStaffInfoView.java
@@ -1,38 +1,36 @@
 package project.GUI.Clinic.Staff;
 
 import project.Database.Locations;
+import project.Entities.Doctor;
 
 import javax.swing.*;
-
+import java.util.Vector;
 
 public class ClinicStaffInfoView extends JFrame {
-/*
-    private ImageIcon imageForBG=new ImageIcon(Locations.getImagePath("clinicInfo.png"));
-    private JLabel backGround=new JLabel(imageForBG);
+    private ImageIcon imageForBG = new ImageIcon(Locations.getImagePath("clinicInfo.png"));
+    private JLabel backGround = new JLabel(imageForBG);
+    
 
-
-     calendar = new JCalendar();
-    JDateChooser chooser = new JDateChooser();
-
-
-    public ClinicStaffInfoView(){
+    public ClinicStaffInfoView() {
         setLayout(null);
         setLocationAndSize();
         addComponentsToFrame();
 
-        setBounds(500, 100, 800, 500);
+        setTitle("Our Doctors: ");
+        setBounds(300, 300, 800, 50);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setResizable(false);
         setVisible(true);
+    }
+
+    private void setLocationAndSize()
+    {
 
     }
 
-    private void setLocationAndSize() {
-        backGround.setBounds(0,0,800,600);
-    }
+    private void addComponentsToFrame()
+    {
 
-    private void addComponentsToFrame() {
         add(backGround);
     }
-*/
 }
