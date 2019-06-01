e00bb566249c6eaafc19f7ed02785c425577c941
Yonatan Yardeni <yoniy93@gmail.com>
2019-06-01 11:15:20 +0300

    לוגיקה לכניסה

--- src/project/GUI/Patient/Treatments/TreatmentsPricesController.java
+++ src/project/GUI/Patient/Treatments/TreatmentsPricesController.java
@@ -1,32 +1,31 @@
 package project.GUI.Patient.Treatments;
 
 import project.Entities.Treatments;
-import project.Database.DBQuerys;
+
+import javax.swing.*;
 
 public class TreatmentsPricesController
 {
     TreatmentsPricesView treatmentsPricesView;
     TreatmentsPricesModel treatmentsPricesModel;
-    TreatmentsPricesController(TreatmentsPricesModel treatmentsPricesModel,TreatmentsPricesView treatmentsPricesView)
-    {
+
+    TreatmentsPricesController(TreatmentsPricesModel treatmentsPricesModel,TreatmentsPricesView treatmentsPricesView) {
         this.treatmentsPricesModel=treatmentsPricesModel;
         this.treatmentsPricesView=treatmentsPricesView;
-
+        treatmentsPricesView.setTreatmentList(new JComboBox(treatmentsPricesModel.getAllTreatments()));
+        treatmentsPricesView.add(treatmentsPricesView.getTreatmentList());
         addViewActionListeners();
     }
-    private void addViewActionListeners()
-    {
+    private void addViewActionListeners() {
         treatmentsPricesView.getTreatmentList().addActionListener(e->treatmentListAction());
 
     }
 
-    private void treatmentListAction()
-    {
+    private void treatmentListAction() {
         int i=treatmentsPricesView.getTreatmentList().getSelectedIndex();
-       Treatments treatmentSelected=treatmentsPricesView.getTreatmentsVector().get(i);
-       double price= ( treatmentSelected.getPrice());
-       treatmentsPricesView.setTreatmentPrices(Double.toString(price));
-
+        Treatments treatmentSelected=treatmentsPricesModel.getTreatmentsVector().get(i);
+        double price= treatmentSelected.getPrice();
+        treatmentsPricesView.setTreatmentPrices(Double.toString(price));
     }
 
 
--- src/project/GUI/Patient/Treatments/TreatmentsPricesView.java
+++ src/project/GUI/Patient/Treatments/TreatmentsPricesView.java
@@ -1,64 +1,40 @@
 package project.GUI.Patient.Treatments;
 
 import project.Database.Locations;
-import project.Entities.Treatments;
-import project.Database.DBQuerys;
-
 import javax.swing.*;
-import java.util.Vector;
 
 public class TreatmentsPricesView extends JFrame {
-
-    private DBQuerys dbQuerys=new DBQuerys();
-    private Vector<Treatments> treatmentsVector=dbQuerys.getTreatments();
-    private String [] treatmentsName;
 
     private ImageIcon imageForBG=new ImageIcon(Locations.getImagePath("searchTreatments.png"));
     private JLabel backGround=new JLabel(imageForBG);
 
-    private JComboBox treatmentList;
+    private JComboBox treatmentList=new JComboBox();
     private JLabel treatmentPrices= new JLabel(" ");
 
-    public TreatmentsPricesView()
-    {
-        setTreatmentsName();
-        treatmentList=new JComboBox(treatmentsName);
+    public TreatmentsPricesView() {
         setLayout(null);
         setLocationAndSize();
         addComponentsToFrame();
 
-
         setTitle("Search Your Treatment: ");
         setBounds(300, 100, 700, 500);
-//        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
         setVisible(true);
     }
 
-    private void setLocationAndSize()
-    {
+    private void setLocationAndSize() {
         treatmentList.setBounds(300, 200, 200, 30);
         treatmentPrices.setBounds(250, 200, 200 , 30);
         backGround.setBounds(0,0,700,500);
     }
 
 
-    private void addComponentsToFrame()
-    {
+    private void addComponentsToFrame() {
         add(treatmentList);
-        add(treatmentPrices);
         add(backGround);
-
     }
 
-    public void setTreatmentsName()
-    {
-        treatmentsName=new String[treatmentsVector.size()];
-        for(int i=0;i<treatmentsVector.size();i++)
-        {
-            treatmentsName[i]=(treatmentsVector.get(i).getName());
-        }
-    }
     public JComboBox getTreatmentList() {
         return treatmentList;
     }
@@ -67,11 +43,12 @@ public class TreatmentsPricesView extends JFrame {
         return treatmentPrices;
     }
 
-    public void setTreatmentPrices(String price)
-    {
+    public void setTreatmentPrices(String price) {
         treatmentPrices.setText((String)price);
     }
-    public Vector<Treatments> getTreatmentsVector() {
-        return treatmentsVector;
+
+    public void setTreatmentList(JComboBox treatmentList) {
+        this.treatmentList = treatmentList;
     }
+
 }
