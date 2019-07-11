f55455c291647e7f93afa9b04144b0afabec76a1
Yonatan Yardeni <yoniy93@gmail.com>
2019-07-11 20:21:49 +0300

    לוגיקה לכניסה

--- src/project/GUI/Clinic/Info/ClinicInfoView.java
+++ src/project/GUI/Clinic/Info/ClinicInfoView.java
@@ -2,7 +2,6 @@ package project.GUI.Clinic.Info;
 
 import project.Database.Locations;
 import project.GUI.General.CancleButton;
-
 import javax.swing.*;
 import java.awt.*;
 
@@ -12,19 +11,21 @@ public class ClinicInfoView extends JFrame {
     private JLabel backGround=new JLabel(imageForBG);
 
     private String[][] tableData = {
-            {"Days:", "Hours:"},
-            {"Sunday", "09:00 - 17:00"},
-            {"Monday", "09:00 - 17:00"},
-            {"Tuesday", "09:00 - 17:00"},
-            {"Wednesday", "08:00 - 15:00"},
-            {"Thursday", "09:00 - 17:00"},
+            {" Days:", "Hours:"},
+            {" Sunday", "09:00 - 17:00"},
+            {" Monday", "09:00 - 17:00"},
+            {" Tuesday", "09:00 - 17:00"},
+            {" Wednesday", "08:00 - 15:00"},
+            {" Thursday", "09:00 - 17:00"},
     };
-
     private String[] columnNames = { "Days:", "Hours:" };
-    private JLabel clinicNameLable = new JLabel( "Dentist Clinic: ");
+    private JLabel clinicNameLable = new JLabel( "<HTML><U>Clinic Info:</U></HTML>");
     private JLabel openningHoursLable = new JLabel( "Opening Hours: ");
-    private JLabel phoneNumberLable = new JLabel( "Phone Numbers: ");
+    private JLabel phoneNumberLable = new JLabel( "Phone Number: ");
     private JLabel addressLabel = new JLabel( "Address: ");
+    private JLabel address = new JLabel( "  Holon, Golomb 52");
+    private JLabel phone1 = new JLabel( "  03-6880321");
+
     private JTable hoursTable = new JTable(tableData,columnNames);
     CancleButton cancleButton=new CancleButton();
 
@@ -33,7 +34,7 @@ public class ClinicInfoView extends JFrame {
         setLocationAndSize();
         addComponentsToFrame();
 
-        setTitle("מידע שימושי");
+        setTitle("Information");
         setBounds(200, 200, 800, 500);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setResizable(false);
@@ -42,35 +43,49 @@ public class ClinicInfoView extends JFrame {
 
     private void setLocationAndSize(){
         clinicNameLable.setHorizontalAlignment(SwingConstants.CENTER);
-        clinicNameLable.setFont(new Font("Ariel", Font.BOLD, 26));
-        clinicNameLable.setBounds(116, 28, 348, 69);
+        clinicNameLable.setFont(new Font("Ariel", Font.BOLD, 30));
+        clinicNameLable.setBounds(180, 28, 348, 80);
 
         openningHoursLable.setHorizontalAlignment(SwingConstants.RIGHT);
-        openningHoursLable.setFont(new Font("Ariel", Font.BOLD, 16));
-        openningHoursLable.setOpaque(true);
-        openningHoursLable.setBackground(Color.lightGray);
-        openningHoursLable.setBounds(20,120,150,40);
+        openningHoursLable.setFont(new Font("Ariel", Font.BOLD, 20));
+        openningHoursLable.setOpaque(false);
+        openningHoursLable.setBounds(20,120,200,40);
 
         phoneNumberLable.setHorizontalAlignment(SwingConstants.RIGHT);
-        phoneNumberLable.setFont(new Font("Ariel", Font.BOLD, 16));
-        phoneNumberLable.setOpaque(true);
-        phoneNumberLable.setBackground(Color.lightGray);
-        phoneNumberLable.setBounds(20,300, 150,40);
+        phoneNumberLable.setFont(new Font("Ariel", Font.BOLD, 20));
+        phoneNumberLable.setOpaque(false);
+        phoneNumberLable.setBounds(20,300, 200,40);
 
         addressLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-        addressLabel.setFont(new Font("Ariel", Font.BOLD, 16));
-        addressLabel.setOpaque(true);
-        addressLabel.setBackground(Color.lightGray);
-        addressLabel.setBounds(420,120,114,40);
+        addressLabel.setFont(new Font("Ariel", Font.BOLD, 20));
+        addressLabel.setOpaque(false);
+        addressLabel.setBounds(380,120,200,40);
 
-        hoursTable.setBounds(180, 130, 230, 120);
+        hoursTable.setBounds(230, 130, 230, 125);
         hoursTable.setFont(new Font("Ariel", Font.BOLD, 16));
-        hoursTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        hoursTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
         hoursTable.setRowHeight(20);
+        hoursTable.setBackground(Color.WHITE);
         hoursTable.getColumnModel().getColumn(0).setPreferredWidth(120);
         hoursTable.getColumnModel().getColumn(1).setPreferredWidth(150);
+        hoursTable.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
 
-        cancleButton.setLocation(220,400,100,30);
+
+        address.setHorizontalAlignment(SwingConstants.LEFT);
+        address.setFont(new Font("Ariel", Font.BOLD, 18));
+        address.setOpaque(true);
+        address.setBackground(Color.WHITE);
+        address.setBounds(580,125,190,30);
+        address.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+
+        phone1.setHorizontalAlignment(SwingConstants.LEFT);
+        phone1.setFont(new Font("Ariel", Font.BOLD, 18));
+        phone1.setOpaque(true);
+        phone1.setBackground(Color.WHITE);
+        phone1.setBounds(220,305,150,30);
+        phone1.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+
+        cancleButton.setLocation(610,400,100,30);
         backGround.setBounds(0,0,800,500);
     }
 
@@ -80,6 +95,8 @@ public class ClinicInfoView extends JFrame {
         add(clinicNameLable);
         add(cancleButton);
         add(addressLabel);
+        add(address);
+        add(phone1);
         add(phoneNumberLable);
         add(openningHoursLable);
         add(backGround);
