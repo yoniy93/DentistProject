c8733b8b8645758248f5bde5a2498cd7050f595e
Yonatan Yardeni <yoniy93@gmail.com>
2019-07-14 21:02:40 +0300

    לוגיקה לכניסה

--- src/project/GUI/Doctor/PatientsHistory/PatientsHistoryView.java
+++ src/project/GUI/Doctor/PatientsHistory/PatientsHistoryView.java
@@ -2,18 +2,16 @@ package project.GUI.Doctor.PatientsHistory;
 
 import project.Database.Locations;
 import project.Entities.Appointment;
-import project.Entities.Patient;
 import project.GUI.General.CancelButton;
-
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 
 public class PatientsHistoryView extends JFrame{
-
 
     private ImageIcon imageForBG=new ImageIcon(Locations.getImagePath("searchTreatments.png"));
     private JLabel backGround=new JLabel(imageForBG);
@@ -23,25 +21,53 @@ public class PatientsHistoryView extends JFrame{
 
     PatientsHistoryController patientsHistoryController=new PatientsHistoryController();
 
-    JTable tbl=new JTable();
-    DefaultTableModel dtm=new DefaultTableModel();
+    JTable appointmentTable =new JTable();
+    DefaultTableModel tablePattern =new DefaultTableModel();
     JScrollPane jScrollPane;
-    String header[] = new String[] { "Date" ,"Treatment", "Time", "Doctor" };
+    String columsNames[] = new String[] { "Treatment" ,"Date", "Time", "Doctor" };
+
+    CancelButton cancelButton=new CancelButton();
+
+    public PatientsHistoryView() {
+        setLayout(null);
+        getPatientJComboBox().setVisible(true);
+        setPatientList(patientsHistoryController.getPatientsList());
+        loadTable();
+        jScrollPane=new JScrollPane(appointmentTable);
+        setLocationAndSize();
+        addComponentsToFrame();
+        addActionListeners();
+
+        setTitle("Patients Treatments History:");
+        setBounds(300, 20, 700, 500);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    private void loadTable(){
+        appointmentTable=updateTableDetails(getSelectedID());
+        appointmentTable.setFont(new Font("Ariel", Font.BOLD, 14));
+    }
 
     private JTable updateTableDetails(String id){
-        dtm.setRowCount(0);
-        dtm.setColumnCount(0);
-        dtm.setColumnIdentifiers(header);
-        tbl.setModel(dtm);
-        // add row dynamically into the table
+        tablePattern.setRowCount(0);
+        tablePattern.setColumnCount(0);
+
+        tablePattern.setColumnIdentifiers(columsNames);
+
+        appointmentTable.setModel(tablePattern);
         List<Appointment> listAppointment=patientsHistoryController.getTreatmentHistory(id);
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        String strDate;
         for (Appointment x : listAppointment) {
-            dtm.addRow(new Object[] { x.getTreatmentDate(),
-                                      getTreatmentName(x.getTreatmentID()),
-                                      x.getTreatmentTime(),
-                                      getDoctorName(x.getDoctorId())} );
+            strDate= dateFormat.format(x.getTreatmentDate());
+            tablePattern.addRow(new Object[] { getTreatmentName(x.getTreatmentID()),
+                    strDate,
+                    x.getTreatmentTime(),
+                    getDoctorName(x.getDoctorId())} );
         }
-        return tbl;
+        return appointmentTable;
     }
 
     private String getTreatmentName(String id){
@@ -50,31 +76,6 @@ public class PatientsHistoryView extends JFrame{
 
     private String getDoctorName(String id){
         return patientsHistoryController.queryDoctorName(id);
-    }
-
-
-    CancelButton cancelButton=new CancelButton();
-
-
-
-
-
-    public PatientsHistoryView() {
-        setLayout(null);
-        this.getPatientJComboBox().setVisible(true);
-        this.setPatientList(patientsHistoryController.getPatientsList());
-        tbl.setBounds(100,100,300,200);
-        tbl=updateTableDetails(getSelectedID());
-        tbl.setFont(new Font("Ariel", Font.BOLD, 14));
-        jScrollPane=new JScrollPane(tbl);
-        setLocationAndSize();
-        addComponentsToFrame();
-        addActionListeners();
-        setTitle("Patients Treatments History:");
-        setBounds(300, 20, 700, 500);
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        setResizable(false);
-        setVisible(true);
     }
 
     private String getSelectedID(){
@@ -86,12 +87,11 @@ public class PatientsHistoryView extends JFrame{
         patientJComboBox.setBounds(50, 100, 200, 50);
 
         selectPatientLabel.setFont(new Font("Ariel", Font.BOLD, 14));
-        selectPatientLabel.setBounds(60,80,100,30);
-        jScrollPane.setBounds(100,200,400,200);
+        selectPatientLabel.setBounds(60,80,200,30);
+        jScrollPane.setBounds(100,190,500,150);
 
-        cancelButton.setLocation(400,400,120,30);
+        cancelButton.setLocation(500,400,120,30);
         backGround.setBounds(0,0,700,500);
-
     }
 
     private void addComponentsToFrame(){
@@ -100,7 +100,6 @@ public class PatientsHistoryView extends JFrame{
         add(selectPatientLabel);
         add(cancelButton);
         add(backGround);
-
     }
 
     public JComboBox getPatientJComboBox() {
