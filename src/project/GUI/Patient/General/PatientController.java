82e8273b8fd640b74d2882bd78d2b8e78e6d1f66
OfirGan <OfirGan@users.noreply.github.com>
2019-07-14 21:41:49 +0300

    Ofir :)

--- src/project/GUI/Patient/General/PatientController.java
+++ src/project/GUI/Patient/General/PatientController.java
@@ -1,10 +1,11 @@
 package project.GUI.Patient.General;
 
+import project.Entities.Patient;
+import project.Entities.Treatments;
 import project.GUI.General.PersonController;
 import project.GUI.Patient.Appointments.SetAnAppointmentView;
-import project.GUI.Patient.Appointments.StartAppointmentView;
-import project.GUI.Patient.PersonalDetails.StartPersonalDetailsPatient;
-import project.GUI.Patient.Treatments.StartTreatmentsPricesView;
+import project.GUI.Patient.PersonalDetails.PersonalDetailsViewPatient;
+import project.GUI.Patient.Treatments.TreatmentsPricesView;
 
 import javax.swing.*;
 
@@ -12,45 +13,98 @@ public class PatientController extends PersonController{
 
     private PatientModel patientModel;
     private PatientView patientView;
+    private SetAnAppointmentView setAnAppointmentView;
+    private PersonalDetailsViewPatient pDetailsView;
+    TreatmentsPricesView treatmentsPricesView;
 
-    public PatientController(PatientModel patientM, PatientView patientV)
-    {
+
+    public PatientController(PatientModel patientM, PatientView patientV) {
         patientView = patientV;
         patientModel = patientM;
+
         addViewActionListeners();
     }
 
     private void addViewActionListeners() {
-
         addActionsToPerson(patientView);
         patientView.setActions(e->editDetailsAction(), e->setAnAppointmentAction(),e->viewTreatmentsAction());
-
-/*
-        patientView.getLogoutButton().addActionListener(e -> logoutAction(patientView));
-        patientView.getEditPersonalDetails().addActionListener(e -> editDetailsAction());
-        patientView.getViewTreatments().addActionListener(e->viewTreatmentsAction());
-        patientView.getViewClinicDetails().addActionListener(e->viewClinicInfo());
-*/
     }
+
 
     private void editDetailsAction() {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new StartPersonalDetailsPatient(patientModel.getPatient());
-            }
-        });
+        pDetailsView =  new PersonalDetailsViewPatient();
+        pDetailsView.setActions(e->editAction(), e-> pDetailsView.dispose());
+        SetTextFieldsCurrentValues();
     }
+
+    private void SetTextFieldsCurrentValues() {
+        pDetailsView.initializeFields(patientModel.getPatient());
+    }
+
+    private void editAction() {
+        Patient patient = patientModel.getPatient();
+
+        patient.setFirstName(pDetailsView.getFirstNameText());
+        patient.setLastName(pDetailsView.getLastNameText());
+        patient.setEmail(pDetailsView.getEmailText());
+        patient.setPassword(pDetailsView.getPasswordText());
+        patient.setHeight(Integer.parseInt(pDetailsView.getHeightText()));
+        patient.setWeight(Double.parseDouble(pDetailsView.getWeightText()));
+        patient.setPhoneNumber(pDetailsView.getPhoneNumberText());
+
+        patientModel.UpdatePatient(patient);
+
+        JOptionPane.showMessageDialog( pDetailsView,  "Details Updated");
+
+        pDetailsView.dispose();
+    }
+
 
     private void setAnAppointmentAction() {
-        new StartAppointmentView(patientModel.getPatient());
+        this.setAnAppointmentView = new SetAnAppointmentView();
 
+        setAnAppointmentView.setDoctorList (patientModel.getDoctorList());
+        setAnAppointmentView.setTreatmentList (patientModel.getTreatmentList());
+
+        setAnAppointmentView.setVisible(true);
+        setAnAppointmentView.setActions(e->selectDoctorAction(), e->insertAppointmentAction(), e->setAnAppointmentView.dispose());
     }
 
+    public void selectDoctorAction(){
+        setAnAppointmentView.setDateListener(setAnAppointmentView.getDoctorID());
+        setAnAppointmentView.clearDateAndTime();
+    }
+
+    public void insertAppointmentAction(){
+        if(setAnAppointmentView.haveEmptyTextFields()) {
+            JOptionPane.showMessageDialog(setAnAppointmentView, "Some Information Haven't Been Field Yet");
+        }
+        else {
+            int appointmentID = patientModel.getLastAppointmentID() + 1;
+            int treatmentID = setAnAppointmentView.getTreatmentID();
+            String date = setAnAppointmentView.getDate();
+            String time = setAnAppointmentView.getTime();
+            String doctorId = setAnAppointmentView.getDoctorID();
+
+            patientModel.insertAppointment(appointmentID, treatmentID, date, time, doctorId);
+
+            JOptionPane.showMessageDialog(setAnAppointmentView, "Appointment has been Schedule");
+            setAnAppointmentView.dispose();
+        }
+    }
+
+
     private void viewTreatmentsAction(){
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new StartTreatmentsPricesView();
-            }
-        });
+        treatmentsPricesView = new TreatmentsPricesView();
+        treatmentsPricesView.setTreatmentList(patientModel.getAllTreatments());
+        treatmentsPricesView.setActions(e->treatmentListAction(),e->treatmentsPricesView.dispose());
+    }
+
+    private void treatmentListAction() {
+        Treatments treatment = treatmentsPricesView.getTreatment();
+
+        if (treatment != null) {
+            treatmentsPricesView.setTreatmentInfo(Double.toString(treatment.getPrice()),Integer.toString(treatment.getDuration()),"None");
+        }
     }
 }
--- src/project/GUI/Patient/General/PatientModel.java
+++ src/project/GUI/Patient/General/PatientModel.java
@@ -1,13 +1,26 @@
 package project.GUI.Patient.General;
 
+import project.Database.DBInserts;
+import project.Database.DBQuerys;
+import project.Database.DBUpdates;
+import project.Entities.Doctor;
 import project.Entities.Patient;
+import project.Entities.Treatments;
+
+import java.util.List;
 
 public class PatientModel {
 
     private Patient patient;
 
-    public PatientModel(Patient patient)
-    {
+    private DBQuerys dbQuerys = new DBQuerys();
+    private DBInserts dbInserts = new DBInserts();
+
+    private List<Doctor> doctorsList;
+    private List<Treatments> treatmentsList;
+
+
+    public PatientModel(Patient patient) {
         this.patient = patient;
     }
 
@@ -15,8 +28,33 @@ public class PatientModel {
         return patient;
     }
 
-    public void setpatient(Patient patient) {
+    public void UpdatePatient(Patient patient) {
+        DBUpdates databaseUpdates = new DBUpdates();
+        databaseUpdates.updatePatientDetails(patient);
         this.patient = patient;
     }
 
+    public List<Treatments> getTreatmentList() {
+        treatmentsList = dbQuerys.getTreatments();
+        return treatmentsList;
+    }
+
+    public List<Doctor> getDoctorList() {
+        doctorsList = dbQuerys.getDoctors();
+        return doctorsList;
+    }
+
+    public void insertAppointment(int appointmentID,int treatmentID,String date, String time, String doctorid){
+
+        dbInserts.insertAppointments(appointmentID, treatmentID, date, time, patient.getId(), doctorid);
+    }
+
+    public int getLastAppointmentID(){
+        return dbQuerys.getLastId("appointments");
+    }
+
+    public List<Treatments> getAllTreatments(){
+        treatmentsList = dbQuerys.getTreatments();
+        return treatmentsList;
+    }
 }
