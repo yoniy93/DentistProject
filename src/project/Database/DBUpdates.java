4bdb10b215fd01f6c8cfe51f490ed564719a5ce3
amitaflalo <amitaflalo01@gmail.com>
2019-07-14 15:48:45 +0300

    שינוי שם לproject

--- src/project/Database/DBUpdates.java
+++ src/project/Database/DBUpdates.java
@@ -2,16 +2,10 @@ package project.Database;
 
 import project.Entities.Admin;
 import project.Entities.Doctor;
-import project.Entities.MedicalEquipment;
 import project.Entities.Patient;
 
 public class DBUpdates {
     DBInitializer dbInitializer=DBInitializer.getInstance();
-
-    public void updateEquipmentsQuantity(MedicalEquipment m){
-        String sql= "UPDATE medical_equipment SET  quantity="+m.getAmountInStock()+"WHERE id=" +m.getId()+";";
-        dbInitializer.connectAndExecute(sql);
-    }
 
     public void updatePatientDetails(Patient p){
         String sql= "UPDATE users SET password='"+p.getPassword()+"', firstname='"+p.getFirstName() +
