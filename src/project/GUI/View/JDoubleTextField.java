3993399c6384d67bd9b4d43710eec65afc1eeea8
OfirGan <OfirGan@users.noreply.github.com>
2019-07-17 18:59:16 +0300

    Ofir :)

--- src/project/GUI/View/JDoubleTextField.java
+++ src/project/GUI/View/JDoubleTextField.java
@@ -3,6 +3,9 @@ package project.GUI.View;
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 
+import static java.awt.event.KeyEvent.VK_DECIMAL;
+import static java.awt.event.KeyEvent.VK_PERIOD;
+
 /**
  * A {@link JTextField} that skips all non-digit keys. The user is only able to enter numbers.
  *
@@ -12,11 +15,28 @@ import java.awt.event.KeyEvent;
 public class JDoubleTextField extends JTextField {
     private static final long serialVersionUID = 1L;
 
+    private static int dotCount = 0;
+
     @Override
     public void processKeyEvent(KeyEvent ev) {
-        if (Character.isAlphabetic(ev.getKeyChar()) || ev.getKeyChar() == KeyEvent.VK_BACK_SPACE) {
+
+        char keyChar = ev.getKeyChar();
+        int keyCode = ev.getKeyCode();
+
+        if (Character.isDigit(keyChar) || keyChar == KeyEvent.VK_BACK_SPACE || keyCode == KeyEvent.VK_PERIOD || keyCode == VK_DECIMAL) {
+            if(keyCode == VK_PERIOD || keyCode == VK_DECIMAL)
+                dotCount++;
+            else
+                super.processKeyEvent(ev);
+
+            if(dotCount > 1){
+                ev.consume();
+                return;
+            }
+
             super.processKeyEvent(ev);
         }
+
         ev.consume();
         return;
     }
